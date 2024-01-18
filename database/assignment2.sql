f88d432033e72ac7f146e7a9451c20e7d9ad3d4e
stephane-lima <ggabriellopess99999@gmail.com>
2024-01-18 16:35:42 -0100

    W02 Assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,30 @@
+-- Query 1
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Query 2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- Query 3
+DELETE FROM account WHERE account_id = 1;
+
+-- Query 4
+UPDATE inventory
+SET inv_description = replace(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+-- WHERE invMake="GM" AND invModel="Hummer";
+
+-- Query 5
+SELECT inv_make, inv_model, classification_name
+FROM inventory
+    JOIN classification
+    ON inventory.classification_id = classification.classification_id
+WHERE inventory.classification_id = 2;
+-- WHERE classification_name = 'Sport';
+
+-- Query 6
+UPDATE inventory
+SET inv_image = replace(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = replace(inv_image, '/images/', '/images/vehicles/');
