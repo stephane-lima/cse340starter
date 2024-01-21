8745f5b39b8a87cd1c0f9cfcad36d79f09966aa0
stephane-lima <ggabriellopess99999@gmail.com>
2024-01-21 15:25:08 -0100

    Inventory Delivery By Classification

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,10 +1,44 @@
 const pool = require("../database/index.js")
+const Util = require("../utilities/index.js")
 
 /* ***************************
     *  Get all classification data
  * ************************** */
 async function getClassifications(){
     return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+}
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+    let grid
+    if(data.length > 0){
+        grid = '<ul id="inv-display">'
+        data.forEach(vehicle => { 
+            grid += '<li>'
+            grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+            + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+            + 'details"><img src="' + vehicle.inv_thumbnail 
+            +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+            +' on CSE Motors" /></a>'
+            grid += '<div class="namePrice">'
+            grid += '<hr />'
+            grid += '<h2>'
+            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+            + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+            grid += '</h2>'
+            grid += '<span>$' 
+            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+            grid += '</div>'
+            grid += '</li>'
+        })
+        grid += '</ul>'
+    } else { 
+        grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+    }
+    return grid
 }
 
 module.exports = {getClassifications}
