53dad01bd6881a0f81dc73e4038d03e030647938
stephane-lima <ggabriellopess99999@gmail.com>
2024-01-22 18:33:45 -0100

    Error Handling Activity

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -8,37 +8,22 @@ async function getClassifications(){
     return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
-/* **************************************
-* Build the classification view HTML
-* ************************************ */
-Util.buildClassificationGrid = async function(data){
-    let grid
-    if(data.length > 0){
-        grid = '<ul id="inv-display">'
-        data.forEach(vehicle => { 
-            grid += '<li>'
-            grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-            + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-            + 'details"><img src="' + vehicle.inv_thumbnail 
-            +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-            +' on CSE Motors" /></a>'
-            grid += '<div class="namePrice">'
-            grid += '<hr />'
-            grid += '<h2>'
-            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-            + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-            grid += '</h2>'
-            grid += '<span>$' 
-            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-            grid += '</div>'
-            grid += '</li>'
-        })
-        grid += '</ul>'
-    } else { 
-        grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+    try {
+        const data = await pool.query(
+            `SELECT * FROM public.inventory AS i 
+            JOIN public.classification AS c 
+            ON i.classification_id = c.classification_id 
+            WHERE i.classification_id = $1`,
+            [classification_id]
+        )
+        return data.rows
+    } catch (error) {
+        console.error("getclassificationsbyid error " + error)
     }
-    return grid
 }
 
-module.exports = {getClassifications}
+module.exports = {getClassifications, getInventoryByClassificationId}
