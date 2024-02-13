eca6dfb04df63d88de1d052bbb61529d1cf66e92
stephane-lima <ggabriellopess99999@gmail.com>
2024-02-12 23:55:17 -0100

    Add show password button and corresponding css

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -38,8 +38,8 @@ function buildInventoryList(data) {
     data.forEach(function (element) { 
         console.log(element.inv_id + ", " + element.inv_model); 
         dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`; 
-        dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`; 
-        dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`; 
+        dataTable += `<td><a class='modify' href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`; 
+        dataTable += `<td><a class='delete' href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`; 
     }) 
     dataTable += '</tbody>';
 
